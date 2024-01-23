a062ea20d1f3924ee69b4d7a19f2b20a9a99cff3
chagchagchag <chagchagchag.dev@gmail.com>
2024-01-23 20:45:24 +0900

    first status

---
+++ nuxt.config.js
@@ -0,0 +1,12 @@
+import { withDocus } from 'docus'
+
+// Learn more at https://docus.dev
+export default withDocus({
+  target: 'static',
+  router: {
+    base: '/fibonacc-backend-docs/'
+  },
+  docus: {
+    // primaryColor: '#E24F55'
+  }
+})
